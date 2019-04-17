885b4cba2e36478d4626f68ebaef5bb758c1b743
Karavaev Arsenii <karavaev@skbkontur.ru>
2019-04-17 21:26:54 +0500

    'init'

---
+++ build/precache-manifest.a52b30f52816f310fbfe2cce5cef3c23.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "7f8b9137799e70a66457",
+    "url": "/static/css/main.f3db8c01.chunk.css"
+  },
+  {
+    "revision": "7f8b9137799e70a66457",
+    "url": "/static/js/main.a85a15e8.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "5090215b38d43c1c8277",
+    "url": "/static/js/2.59f5efd6.chunk.js"
+  },
+  {
+    "revision": "f4df4a965597012a48bf333723eb5ae8",
+    "url": "/index.html"
+  }
+];
